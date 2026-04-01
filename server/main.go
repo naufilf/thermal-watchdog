74664554465edd010f72dc163e0a820dca36d25e
Naufil Faruqi <naufilfaruqi@gmail.com>
2026-04-01 17:32:27 -0400

    setup bazel build & initial population of proto

--- server/main.go
+++ server/main.go
@@ -1,10 +1,11 @@
 package main
 
 import (
+	"fmt"
 	pb "github.com/naufilf/thermal-watchdog/proto/telemetry"
 )
 
 func main() {
 	report := &pb.TelemetryReport{NodeId: "test-node-01"}
-	fmf.Printf(("Bazel is working Node: %s\n"), report.NodeId)
+	fmt.Printf(("Bazel is working Node: %s\n"), report.NodeId)
 }
